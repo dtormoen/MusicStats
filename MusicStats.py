fa67bc6110315ecba092683ae0b5cd59edf08d96
tormo020 <tormo020@umn.edu>
2013-09-30 16:59:17 -0500

    Added statistics based on time spent listening to music rather than only play counts. Simplified main for loop that sorts through songs. Formatting of output is terrible so that will be fixed next

--- MusicStats.py
+++ MusicStats.py
@@ -6,37 +6,76 @@ import operator
 iTunesLib = plistlib.readPlist('iTunes Music Library.xml')
 tracks = iTunesLib['Tracks']
 
+def intToTime(time):
+	milli = time % 1000
+	time = time / 1000
+	seconds = time % 60
+	time = time / 60
+	minutes = time % 60
+	time = time / 60
+	hours = time % 24
+	time = time / 24
+	days = time 
+	return '{}:{}:{}:{}.{}'.format(days,hours,minutes,seconds,milli)
+
+def updateValue(track, value):
+	if 'Play Count' in track:
+		value[0] += track['Play Count']
+		if 'Total Time' in track:
+			value[1] += track['Total Time'] * track['Play Count']
+
+
+displayNum = 10
+
 artists = dict()
 albums = dict()
 totalPlays = 0
+totalTime = 0
 for trackId, track in tracks.iteritems():
 	if 'Play Count' in track:
 		totalPlays += track['Play Count']
+		if 'Total Time' in track:
+			totalTime += track['Total Time'] * track['Play Count']
 		if 'Artist' in track:
-			if track['Artist'] in artists:
-				artists[track['Artist']] += track['Play Count']
-			else:
-				artists[track['Artist']] = track['Play Count'] 
+			updateValue(track, artists.setdefault(track['Artist'],[0,0]))
 		if 'Album' in track:
-			if track['Album'] in albums:
-				albums[track['Album']] += track['Play Count']
-			else:
-				albums[track['Album']] = track['Play Count']
+			updateValue(track, albums.setdefault(track['Album'],[0,0]))
 
+print "Total time:", intToTime(totalTime)
+
+print "\n"
 
 print "Total number of plays:", totalPlays
 
 print "\n"
 
-artistList = sorted(artists.iteritems(), key=operator.itemgetter(1), reverse=True)
-print "Top played artists"
-for artist in artistList[:50]:
-	print artist[0], "-" , artist[1], "-", (artist[1]/float(totalPlays))*100,"%"
+artistList = sorted(artists.iteritems(), key=lambda artist: artist[1][0], reverse=True)
+print "Top played artists by play count"
+for artist in artistList[:displayNum]:
+	print artist[0], "-" , artist[1][0], "-", (artist[1][0]/float(totalPlays))*100,"%"
 
 print "\n"
 
-albumList = sorted(albums.iteritems(), key=operator.itemgetter(1), reverse=True)
-print "Top played albums"
-for album in albumList[:50]:
-	print album[0], "-" , album[1], "-", (album[1]/float(totalPlays))*100,"%"
+albumList = sorted(albums.iteritems(), key=lambda album: album[1][0], reverse=True)
+print "Top played albums by play count"
+for album in albumList[:displayNum]:
+	print album[0], "-" , album[1][0], "-", (album[1][0]/float(totalPlays))*100,"%"
 
+print "\n"
+
+artistList = sorted(artists.iteritems(), key=lambda artist: artist[1][1], reverse=True)
+print "Top played artists by time"
+for artist in artistList[:displayNum]:
+	print artist[0], "-" , intToTime(artist[1][1]), "-", (artist[1][1]/float(totalTime))*100,"%"
+
+print "\n"
+
+albumList = sorted(albums.iteritems(), key=lambda album: album[1][1], reverse=True)
+print "Top played albums by time"
+for album in albumList[:displayNum]:
+	print album[0], "-" , intToTime(album[1][1]), "-", (album[1][1]/float(totalTime))*100,"%"
+
+#Stats to add:
+#	Play density: number of plays/total time of music. Shows which bands are listened to a lot
+#		despite having a small number of songs
+#	Most listened to song: Show top songs listed by how much time has been spenting listening
