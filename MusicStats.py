3694ad6da222f029aa1fec159ce0f9a82c8f3d31
tormo020 <tormo020@umn.edu>
2013-09-30 14:11:12 -0500

    Changes formatting to be more clear in code. Keeps track of the total number of plays in your library. Displays each band and album as a percentage of the total number of plays

--- MusicStats.py
+++ MusicStats.py
@@ -8,8 +8,10 @@ tracks = iTunesLib['Tracks']
 
 artists = dict()
 albums = dict()
+totalPlays = 0
 for trackId, track in tracks.iteritems():
 	if 'Play Count' in track:
+		totalPlays += track['Play Count']
 		if 'Artist' in track:
 			if track['Artist'] in artists:
 				artists[track['Artist']] += track['Play Count']
@@ -22,14 +24,19 @@ for trackId, track in tracks.iteritems():
 				albums[track['Album']] = track['Play Count']
 
 
+print "Total number of plays:", totalPlays
+
+print "\n"
+
 artistList = sorted(artists.iteritems(), key=operator.itemgetter(1), reverse=True)
 print "Top played artists"
-for artist in artistList[:20]:
-	print artist[0], "-" , artist[1]
+for artist in artistList[:50]:
+	print artist[0], "-" , artist[1], "-", (artist[1]/float(totalPlays))*100,"%"
+
+print "\n"
 
 albumList = sorted(albums.iteritems(), key=operator.itemgetter(1), reverse=True)
-print "\n\nTop played albums"
-for album in albumList[:20]:
-	print album[0], "-" , album[1]
-# print albumList[:10]
+print "Top played albums"
+for album in albumList[:50]:
+	print album[0], "-" , album[1], "-", (album[1]/float(totalPlays))*100,"%"
 
